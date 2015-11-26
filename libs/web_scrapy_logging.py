b5c0d2dfe22c0c8b2b5849e97c8ae870328a8247
Price Tseng <Everett6802@hotmail.com>
2015-11-26 08:44:18 +0800

    Fix some issues

--- libs/web_scrapy_logging.py
+++ libs/web_scrapy_logging.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+import os
 import logging
 
 LOG_FILE_FOLDER = "log"
@@ -7,6 +8,10 @@ LOG_FILE_NAME = "web_scrapy.log"
 LOG_FILE_PATH = "%s/%s" % (LOG_FILE_FOLDER, LOG_FILE_NAME)
 
 def get_web_scrapy_logger(level=logging.DEBUG, filename=LOG_FILE_PATH):
+# Create the folder for log file if it does NOT exist
+    if not os.path.exists(LOG_FILE_FOLDER):
+        os.makedirs(LOG_FILE_FOLDER)
+
     logging.basicConfig(filename=filename, level=level, format='%(asctime)-15s %(filename)s:%(lineno)d [%(levelname)s]: %(message)s')
 # Log to console
     # console = logging.StreamHandler()
