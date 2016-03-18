04552e8c13eb6c5c4f9da70957d5020770662d29
Price Tseng <Everett6802@hotmail.com>
2016-03-18 13:36:11 +0800

    Add the scripts of scrapying data on certain a date or a range of date

---
+++ script/scrap_date.sh
@@ -0,0 +1,24 @@
+# !/bash/sh
+
+usage() { echo "Usage: $0 [-d <date_string> (Ex: 2016-01-01)]" 1>&2; exit 1; }
+
+while getopts ":d:" o; do
+    case "${o}" in
+        d)
+            d=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${d}" ]; then
+    usage
+fi
+
+#echo "d = ${d}"
+
+cd /home/super/Projects/finance_scrapy_python
+python ./finance_scrapy.py -m USER_DEFINED -t ${d},${d} --remove_old --check_result
---
+++ script/scrap_date_range.sh
@@ -0,0 +1,28 @@
+# !/bash/sh
+
+usage() { echo "Usage: $0 [-s <start_date_string> (Ex: 2016-01-01)]  [-e <end_date_string> (Ex: 2016-01-01)]" 1>&2; exit 1; }
+
+while getopts ":s:e:" o; do
+    case "${o}" in
+        s)
+            s=${OPTARG}
+            ;;
+        e)
+            e=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${s}" ] || [ -z "${e}" ]; then
+    usage
+fi
+
+#echo "s = ${s}"
+#echo "e = ${e}"
+
+cd /home/super/Projects/finance_scrapy_python
+python ./finance_scrapy.py -m USER_DEFINED -t ${s},${e} --remove_old --check_result
