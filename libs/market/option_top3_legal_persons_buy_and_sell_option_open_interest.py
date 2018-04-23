06288f1224483e6b6153a3d873b02a150b61d5db
price@vm2 <everett6802@hotmail.com>
2018-04-23 11:10:55 +0800

    Add the column name config file for each method

--- libs/market/option_top3_legal_persons_buy_and_sell_option_open_interest.py
+++ libs/market/option_top3_legal_persons_buy_and_sell_option_open_interest.py
@@ -46,6 +46,7 @@ class OptionTop3LegalPersonsBuyAndSellOptionOpenInterest(ScrapyMarketBase.Scrapy
                 data_list.append(element)
             row_index += 1
         return data_list
+# "日期",
 # "買權_自營商_買方_口數",
 # "買權_自營商_買方_契約金額",
 # "買權_自營商_賣方_口數",
