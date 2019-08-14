989cca537a07fb93b96f31e1b14d10a0891baf1d
price@vm2 <everett6802@hotmail.com>
2019-08-14 13:51:06 +0800

    Convert old institutional investor net buy sell data

--- dataset/dataset_conversion.py
+++ dataset/dataset_conversion.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+import re
 import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
@@ -22,10 +23,7 @@ def dataset_conversion_vix(src_filepath, dst_filepath):
         [month_value, day_value, year_value] = map(int, orig_date_str.split("/"))
         return CMN.FUNC.transform_date_str(year_value, month_value, day_value)
 #     import pdb; pdb.set_trace()
-# # Read data from the source file
-#     df = pd.read_csv(src_filepath, **kwargs)
-#     df_pct_change = df["Close"].pct_change()
-#     df["Change%"] = df_pct_change["Close"]
+# Read data from the source file
     kwargs = {
         "names": ["Date", "Open", "High", "Low", "Close",], 
         "skiprows": 1,
@@ -42,3 +40,35 @@ def dataset_conversion_vix(src_filepath, dst_filepath):
     df["Change%"] = df["Change"].apply(lambda x: "%.2f" % (x * 100.0))
 # Write data into the destination file
     df.to_csv(dst_filepath, columns=["Close", "Change%",])
+
+# data source: 
+# https://goodinfo.tw/StockInfo/ShowBuySaleChart.asp?STOCK_ID=2458&CHT_CAT=DATE
+# 顯示範圍: 一年=> export and copy the data from *.xls to *.csv manually
+def dataset_conversion_institutional_investor_net_buy_sell(src_filepath, dst_filepath):
+    def transform_date_str(orig_date_str):
+        # import pdb; pdb.set_trace()
+        mobj = re.match("([\d]{2})'([\d]{2})/([\d]{1,2})", orig_date_str)
+        if mobj is None:
+            raise RuntimeError("Incorrect date string format: %s" % orig_date_str)
+        year_value = int(mobj.group(1)) + 2000
+        month_value = int(mobj.group(2))
+        day_value = int(mobj.group(3))
+        return CMN.FUNC.transform_date_str(year_value, month_value, day_value)
+    # import pdb; pdb.set_trace()
+    COLUMN_NUM = 19
+    column_name_list = ["Date",]
+    column_name_list.extend(["col%d" % i for i in range(1, COLUMN_NUM)])
+# Read data from the source file
+    kwargs = {
+        "names": column_name_list, 
+        "sep": "\s+",
+        "index_col": "Date",
+        "parse_dates": [0,],
+        "date_parser": lambda x: transform_date_str(x),
+    }
+    # import pdb; pdb.set_trace()
+# Read data from the source file
+    df = pd.read_csv(src_filepath, **kwargs)
+    df.sort_index(inplace=True)
+# Write data into the destination file
+    df.to_csv(dst_filepath)
--- dataset_utility.py
+++ dataset_utility.py
@@ -8,9 +8,7 @@ import time
 import scrapy.common as CMN
 from scrapy.common.common_variable import GlobalVar as GV
 import dataset as DS
-# import scrapy.libs.company_profile as CompanyProfile
-# import scrapy.libs.company_group_set as CompanyGroupSet
-# import libs.stock.stock_url_time_range as URLTimeRange
+
 param_cfg = {}
 g_logger = CMN.LOG.get_logger()
 
@@ -19,6 +17,7 @@ def show_usage_and_exit():
     print "====================== Usage ======================"
     print "-h --help\nDescription: The usage\nCaution: Ignore other parameters when set"
     print "--convert_vix\nDescription: Convert VIX history data to the standard dataset format"
+    print "--convert_institutional_investor_net_buy_sell\nDescription: Convert Institutional Investor Net Buy Sell history data to the standard dataset format"
     print "  Format: source_filepath destination_filepath (ex. /home/super/Downloadsvixcurrent.csv /home/super/source/finance_dataset/data/market/vix_history.csv)"
     sys.exit(0)
 
@@ -36,11 +35,18 @@ def convert_vix_and_exit():
     sys.exit(0)
 
 
+def convert_institutional_investor_net_buy_sell_and_exit():
+    [src_filepath, dst_filepath] = param_cfg["convert_institutional_investor_net_buy_sell"]
+    DS.CV.dataset_conversion_institutional_investor_net_buy_sell(src_filepath, dst_filepath)
+    sys.exit(0)
+
+
 def init_param():
     # import pdb; pdb.set_trace()
     global param_cfg
     param_cfg["help"] = False
     param_cfg["convert_vix"] = None
+    param_cfg["convert_institutional_investor_net_buy_sell"] = None
 
 
 def parse_param():
@@ -62,6 +68,11 @@ def parse_param():
             param_cfg["convert_vix"] = []
             param_cfg["convert_vix"].append(sys.argv[index + 1])
             param_cfg["convert_vix"].append(sys.argv[index + 2])
+            index_offset = 3
+        elif sys.argv[index] == "--convert_institutional_investor_net_buy_sell":
+            param_cfg["convert_institutional_investor_net_buy_sell"] = []
+            param_cfg["convert_institutional_investor_net_buy_sell"].append(sys.argv[index + 1])
+            param_cfg["convert_institutional_investor_net_buy_sell"].append(sys.argv[index + 2])
             index_offset = 3
         else:
             show_error_and_exit("Unknown Parameter: %s" % sys.argv[index])
@@ -85,4 +96,5 @@ if __name__ == "__main__":
 # Renew the company data
     if param_cfg.get("convert_vix", None) is not None:
         convert_vix_and_exit()
- 
+    if param_cfg.get("convert_institutional_investor_net_buy_sell", None) is not None:
+        convert_institutional_investor_net_buy_sell_and_exit()
